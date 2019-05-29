e2a273a43d7fa48eb19ff7cbbe403a1ff0289196
hatoo <hato2000@gmail.com>
2019-05-29 23:31:11 +0900

    Update

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ```bash
-running 2 tests
-test bench_fnv_1e5        ... bench:   1,142,098 ns/iter (+/- 279,934)
-test bench_rustc_hash_1e5 ... bench:      10,288 ns/iter (+/- 1,871)
+running 6 tests
+test bench_default_hasher         ... bench:   1,575,812 ns/iter (+/- 1,808,398)
+test bench_default_hasher_80bytes ... bench:   5,788,185 ns/iter (+/- 1,348,687)
+test bench_fnv                    ... bench:   1,046,691 ns/iter (+/- 252,584)
+test bench_fnv_80bytes            ... bench:  10,658,850 ns/iter (+/- 1,476,202)
+test bench_rustc_hash             ... bench:       9,749 ns/iter (+/- 1,419)
+test bench_rustc_hash_80bytes     ... bench:   1,615,360 ns/iter (+/- 329,219)
 
-test result: ok. 0 passed; 0 failed; 0 ignored; 2 measured; 0 filtered out
+test result: ok. 0 passed; 0 failed; 0 ignored; 6 measured; 0 filtered out
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,60 @@
 
 extern crate test;
 
-use test::Bencher;
+use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
+use test::Bencher;
 
-#[bench]
-fn bench_rustc_hash_1e5(b: &mut Bencher) {
-    b.iter(|| {
-        let mut v = test::black_box(114514);
-        for _ in 0..100000 {
-            let mut hasher = rustc_hash::FxHasher::default();
+#[inline]
+fn bench_hasher<T: Hasher + Default>() -> u64 {
+    let mut v: u64 = test::black_box(114514);
+    for _ in 0..100000 {
+        let mut hasher = T::default();
+        hasher.write_u64(v);
+        v = hasher.finish();
+    }
+    v
+}
+
+#[inline]
+fn bench_hasher_80bytes<T: Hasher + Default>() -> u64 {
+    let mut v: u64 = test::black_box(114514);
+    for _ in 0..100000 {
+        let mut hasher = T::default();
+        for _ in 0..10 {
             hasher.write_u64(v);
-            v = hasher.finish();
         }
-        v
-    });
+        v = hasher.finish();
+    }
+    v
 }
 
 #[bench]
-fn bench_fnv_1e5(b: &mut Bencher) {
-    b.iter(|| {
-        let mut v = test::black_box(114514);
-        for _ in 0..100000 {
-            let mut hasher = fnv::FnvHasher::default();
-            hasher.write_u64(v);
-            v = hasher.finish();
-        }
-        v
-    });
+fn bench_default_hasher(b: &mut Bencher) {
+    b.iter(|| bench_hasher::<DefaultHasher>());
+}
+
+#[bench]
+fn bench_rustc_hash(b: &mut Bencher) {
+    b.iter(|| bench_hasher::<rustc_hash::FxHasher>());
+}
+
+#[bench]
+fn bench_fnv(b: &mut Bencher) {
+    b.iter(|| bench_hasher::<fnv::FnvHasher>());
+}
+
+#[bench]
+fn bench_default_hasher_80bytes(b: &mut Bencher) {
+    b.iter(|| bench_hasher_80bytes::<DefaultHasher>());
+}
+
+#[bench]
+fn bench_rustc_hash_80bytes(b: &mut Bencher) {
+    b.iter(|| bench_hasher_80bytes::<rustc_hash::FxHasher>());
+}
+
+#[bench]
+fn bench_fnv_80bytes(b: &mut Bencher) {
+    b.iter(|| bench_hasher_80bytes::<fnv::FnvHasher>());
 }
